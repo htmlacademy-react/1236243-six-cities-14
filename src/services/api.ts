49db94b74fc443c6d87ffbf1d32896e070839152
Iakovlev Igor <iakovlev.igor.dev@gmail.com>
2023-11-17 18:37:12 +0300

    Add react-toastify

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
 import { getToken } from './token';
 import { StatusCodes } from 'http-status-codes';
-import { processErrorHandle } from './process-erroe-handle';
+import { toast } from 'react-toastify';
 
 export type DetailMessageType = {
   type: string;
@@ -42,7 +42,7 @@ export const createAPI = (): AxiosInstance => {
       if (error.response && shouldDisplayError(error.response)) {
         const detailMessage = (error.response.data);
 
-        processErrorHandle(detailMessage.message);
+        toast.warn(detailMessage.message);
       }
 
       return Promise.reject(error);
